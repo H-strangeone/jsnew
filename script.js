24cdd0b8f3a2e3de7f28b9d5b369018566935da6
H-strangeone <hiteshgitman@gmail.com>
2024-04-15 09:50:51 -0700

    i

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 'use strict';
+const supnumber= Math.trunc(Math.random()*20) + 1;
+let count=Number(document.querySelector('.score').textContent);
+document.querySelector('.number').textContent=supnumber;
+while(count>0){
+    document.querySelector('.check').addEventListener('click', function(){
+        const guess=Number(document.querySelector('.guess').value);
+        if(!guess){
+            alert("put a number");
+            document.querySelector('.message').textContent="not a number";
+        }
+        else if(guess>Number(supnumber)){
+            alert("no");
+            count=count-1;
+            document.querySelector('.score').textContent=count;
+            document.querySelector('.message').textContent="lower than that";
+        }
+        else if(guess<Number(supnumber)){
+            alert("no");
+            count=count-1;
+            document.querySelector('.score').textContent=count;
+            document.querySelector('.message').textContent="higher than that";
+        }
+        else if(guess===Number(supnumber)){
+            
+        }
+    });
+}
