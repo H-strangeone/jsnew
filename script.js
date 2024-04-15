9ba9bada2761cd359f0868265f327309e663db40
H-strangeone <hiteshgitman@gmail.com>
2024-04-15 10:28:03 -0700

    i

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 const supnumber= Math.trunc(Math.random()*20) + 1;
 let count=Number(document.querySelector('.score').textContent);
 document.querySelector('.number').textContent=supnumber;
-while(count>0){
+while(count!=0){
     document.querySelector('.check').addEventListener('click', function(){
         const guess=Number(document.querySelector('.guess').value);
         if(!guess){
@@ -10,19 +10,18 @@ while(count>0){
             document.querySelector('.message').textContent="not a number";
         }
         else if(guess>Number(supnumber)){
-            alert("no");
             count=count-1;
             document.querySelector('.score').textContent=count;
             document.querySelector('.message').textContent="lower than that";
         }
         else if(guess<Number(supnumber)){
-            alert("no");
             count=count-1;
             document.querySelector('.score').textContent=count;
             document.querySelector('.message').textContent="higher than that";
         }
-        else if(guess===Number(supnumber)){
-            
+        else{
+            document.querySelector('.message').textContent="correct";
+            document.querySelector('.highscore').textContent=count;
         }
     });
 }
